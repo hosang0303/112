2ec485bafafca617a6322fec14f731e84867af19
hosang0303 <widetaste03@naver.comgit config --global user.email widetaste03@naver.com>
2021-06-27 21:24:26 +0900

    saved

---
+++ src/학생2.java
@@ -0,0 +1,13 @@
+public class 학생2 {
+	String name;
+	String number;
+	int birthday;
+
+
+	public static void main(String[] args) {
+		학생2 s1 = new 학생2();
+		s1.name = "이호상";
+		s1.number = "100303";
+
+	}
+}
